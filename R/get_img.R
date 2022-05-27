a5effb35d76fd8f3ab2ffa603f6152f8c1a8607f
Iain Moppett <iain.moppett@gmail.com>
2022-05-27 15:10:37 +0100

    Re-write of get_img function

--- R/get_img.R
+++ R/get_img.R
@@ -1,34 +1,21 @@
 #' Create a graphical object (GROB) from png or jpg
 #'
 #' @param filename Filepath to png or jpg file.
-#' @param FUN Import function. Switches internally between \code{png::readPNG} and \code{jpeg::readJPEG}
-#' @inheritParams grid::rasterGrob
-#'
 #'
 #' @return A \code{GROB} that can be added to a ggplot.
 #'
 #' @importFrom grid unit
 
-get_img <- function(filename,
-                    FUN = png::readPNG,
-                    x = unit(0.5, "npc"),
-                    y= unit(0.5, "npc"),
-                    width = NULL,
-                    height = NULL,
-                    just = "centre",
-                    vjust = NULL,
-                    hjust = NULL,
-                    gp = grid::gpar()){
+get_img <- function(filename){
   file_ext <- tools::file_ext(filename)
   FUN <- switch(
     file_ext,
     "png" = png::readPNG,
     "jpg" = jpeg::readJPEG,
     "jpeg" = jpeg::readJPEG,
-    print("Only jpg, png and jpeg are allowed")
+    stop("Only jpg, png and jpeg are allowed")
   )
   grid::rasterGrob(FUN(filename), interpolate = TRUE,
-                   x = x, y = y,
-                   width = width, height = height,
-                   gp = gp, hjust = 0.5, vjust = vjust)
+                   x = 0.99,
+                   hjust = 1)
 }
